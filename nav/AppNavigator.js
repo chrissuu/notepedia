0ff560b13770fbe1aba87581f2286f9a36c63050
chrissuu <102528557+chrissuu@users.noreply.github.com>
2023-08-10 00:48:13 -0400

    prelim changes for big ui overhaul

--- nav/AppNavigator.js
+++ nav/AppNavigator.js
@@ -9,16 +9,41 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../components/HomeScreenComponents/HomeScreen';
 import Whiteboard from '../components/WhiteboardComponents/Whiteboard';
 import NotebookTab from '../components/HomeScreenComponents/NotebookTab';
-
-
-
+import PagesHolder from '../components/HomeScreenComponents/NotebookTab';
+import Header from '../components/WhiteboardComponents/Header';
+import LoadingScreen from '../components/LoadingScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   
   return (
-      <NotebookTab /> 
+    <Stack.Navigator 
+      initialRouteName="Loading"
+   
+    >
+      <Stack.Screen
+        name = "Notebooks Screen"
+        component = {PagesHolder}
+        options={{
+          header: () => <Header/>,
+          animationEnabled:false
+        }}
+      />  
+      <Stack.Screen
+        name ="Loading"
+        component = {LoadingScreen}
+        options={{ 
+          headerShown: false,
+          animationEnabled:false 
+        }} 
+      />
+      <Stack.Screen
+        name = "Graphs Screen"
+        component = {HomeScreen}
+      />
+
+    </Stack.Navigator>
       // <Whiteboard identification = '1'/>
       
   
